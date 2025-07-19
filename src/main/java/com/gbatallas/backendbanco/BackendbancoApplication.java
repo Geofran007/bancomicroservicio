825040a2717664a4915abcf7ad36b5bc8acf08f5
Geofran007 <89365653+Geofran007@users.noreply.github.com>
2025-07-19 11:37:24 -0500

    Initial commit

---
+++ src/main/java/com/gbatallas/backendbanco/BackendbancoApplication.java
@@ -0,0 +1,13 @@
+package com.gbatallas.backendbanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendbancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbatallas/backendbanco/BackendbancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbatallas.backendbanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
